7767d3327b2a69ead628eb4cd67fdee7388c7a06
Pavel Polyakov <me@pavelpolyakov.com>
2023-06-28 20:09:47 +0200

    adjust example, add test

--- src/__tests__/render-fails.test.tsx
+++ src/__tests__/render-fails.test.tsx
@@ -1,35 +1,55 @@
-import { render, screen, fireEvent } from '@testing-library/react-native';
+import { render, screen, fireEvent } from "@testing-library/react-native";
 
 import {
-    NativeBaseProvider,
-    Box,
-    TextArea,
-    Heading,
-    Button,
-    Text,
-  } from "native-base";
-  
-  import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
-  
-  function App() {
-    return (
-      <NativeBaseProvider>
-            <Box safeArea>
-              <Heading>What was great last week?</Heading>
-              <TextArea
-                h={20}
-                placeholder="Text Area Placeholder"
-                w="75%"
-                maxW="300"
-                autoCompleteType={false}
-              />
-              <Button onPress={() => {}}>Create New Blog</Button>
-            </Box>
-      </NativeBaseProvider>
-    );
-  }
-  
+  NativeBaseProvider,
+  Box,
+  TextArea,
+  Heading,
+  Button,
+  Text,
+} from "native-base";
 
-test('renders the landing page', () => {
-    render(App());
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+function App() {
+  return (
+    <NativeBaseProvider>
+      <Authenticator.Provider>
+        <Authenticator
+          components={{
+            SignUp: ({ fields, ...props }) => (
+              <Authenticator.SignUp
+                {...props}
+                fields={[
+                  {
+                    name: "custom:displayName",
+                    label: "Display Name",
+                    type: "default",
+                    placeholder: "Enter your display name",
+                  },
+                  ...fields,
+                ]}
+              />
+            ),
+          }}
+        >
+          <Box safeArea>
+            <Heading>What was great last week?</Heading>
+            <TextArea
+              h={20}
+              placeholder="Text Area Placeholder"
+              w="75%"
+              maxW="300"
+              autoCompleteType={false}
+            />
+            <Button onPress={() => {}}>Create New Blog</Button>
+          </Box>
+        </Authenticator>
+      </Authenticator.Provider>
+    </NativeBaseProvider>
+  );
+}
+
+test("renders the landing page", () => {
+  render(App());
 });

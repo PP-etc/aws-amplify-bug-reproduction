45de3950d6d14cd8b25b51c5f4f75ffb3b83ae80
Pavel Polyakov <me@pavelpolyakov.com>
2023-06-28 17:28:30 +0200

    simplify

--- src/__tests__/render-fails.test.tsx
+++ src/__tests__/render-fails.test.tsx
@@ -14,25 +14,6 @@ import {
   function App() {
     return (
       <NativeBaseProvider>
-        <Authenticator.Provider>
-          <Authenticator
-            components={{
-              SignUp: ({ fields, ...props }) => (
-                <Authenticator.SignUp
-                  {...props}
-                  fields={[
-                    {
-                      name: "custom:displayName",
-                      label: "Display Name",
-                      type: "default",
-                      placeholder: "Enter your display name",
-                    },
-                    ...fields,
-                  ]}
-                />
-              ),
-            }}
-          >
             <Box safeArea>
               <Heading>What was great last week?</Heading>
               <TextArea
@@ -44,8 +25,6 @@ import {
               />
               <Button onPress={() => {}}>Create New Blog</Button>
             </Box>
-          </Authenticator>
-        </Authenticator.Provider>
       </NativeBaseProvider>
     );
   }
